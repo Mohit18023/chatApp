da78b8c9b7257316578eee03bcdcb7c93352fde3
mohit choudhary <mohitchoudhary1054@gmail.com>
2023-08-14 00:25:49 +0530

    log in page complete

--- frontend/src/components/Authentication/Login.jsx
+++ frontend/src/components/Authentication/Login.jsx
@@ -7,16 +7,70 @@ import {
   InputGroup,
   InputRightElement,
   VStack,
+  useToast,
 } from "@chakra-ui/react";
 import React, { useState } from "react";
+import axios from "axios";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 export default function Login() {
   const [password, setPassword] = useState();
   const [show, setShow] = useState(false);
   const [email, setEmail] = useState();
-  const handleClick = () => setShow(!show);
+  const [loading, setLoading] = useState(false);
+  const toast = useToast();
+  const history = useHistory();
 
-  const submitHandler = () => {};
+  const handleClick = () => setShow(!show);
+  const submitHandler = async () => {
+    setLoading(true);
+    if(!email || !password) {
+      toast({
+        title:"Please fill all the fields",
+        status:"warning",
+        duration: 5000,
+        isClosable: true,
+        position: "top",
+      });
+      setLoading(false);
+      return;
+    }
+
+    try{
+      const config = {
+        headers: {
+          "Content-type" : "application/json",
+        },
+      };
+      const {data} = await axios.post(
+        "/api/user/login",
+        { email, password },
+        config
+      );
+
+      toast({
+        title: "Login Successful",
+        status:"success",
+        duration:5000,
+        isClosable: true,
+        position: "top"
+      });
+      localStorage.setItem("userInfo",JSON.stringify(data));
+      setLoading(false);
+      history.push("/chats");
+    } catch(err) {
+        toast({
+          title: "Error Occured!",
+          description: err.response.data.message,
+          status: "error",
+          duration: 5000,
+          isClosable: true,
+          position: "top"
+
+        });
+        setLoading(false);
+    }
+  };
   return (
     <VStack spacing={5}>
       <FormControl id="email" isRequired>
@@ -60,6 +114,7 @@ export default function Login() {
         colorScheme="green"
         style={{ marginTop: 10 }}
         onClick={submitHandler}
+        isLoading={loading}
       >
         Submit
       </Button>
